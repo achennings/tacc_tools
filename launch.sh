ec6b4585c5425a0ab34f19de78ac13061488fdd6
achennings <achennings@utexas.edu>
2019-10-08 12:14:29 -0500

    _

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #for subI in 01 02 03 04 05 06 07 08 09 10 12 13 14 15 16 17 18 19 20 21
 #do
 #	launch -N 3 -n 3 -J er1_${subI} -s er_Sub0${subI}.txt -m achennings@utexas.edu -p normal -r 6:00:00 -A LewPea_MRI_Analysis

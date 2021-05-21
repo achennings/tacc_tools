c485e609ea0e6ea88a4e4260866126c3aa19439d
achennings <achennings@utexas.edu>
2021-05-20 21:45:59 -0500

    updated versions

--- build_singularity_img.sh
+++ build_singularity_img.sh
@@ -1,21 +1,19 @@
 # create the fmriprep and MRIqc singularity images on TACC allocation
 # puts the images in a folder in $WORK called bids-apps
 
-module load tacc-singularity
-module load python3
-
-DEST=${WORK}/bids-apps
+DEST=${SCRATCH}/bids-apps
 mkdir -p ${DEST}
 
-FMRIPREP_VERS=1.4.0
-MRIQC_VERS=0.15.0
+FMRIPREP_VERS=20.2.1
+MRIQC_VERS=0.16.1
 #for all intents and purposes this should work but something is wrong with my python version and doesn't run for me, try it anyways...
 
-SINGULARITY_NOHTTPS=yes singularity build \
-    ${DEST}/fmriprep.${FMRIPREP_VERS}.simg \
-    docker://poldracklab/fmriprep:${FMRIPREP_VERS}
+SINGULARITY_NOHTTPS=yes 
 
 singularity build \
-    ${DEST}/mriqc.${MRIQC_VERS}.simg \
-    docker://poldracklab/mriqc:${MRIQC_VERS}
+    ${DEST}/fmriprep.simg \
+    docker://nipreps/fmriprep:${FMRIPREP_VERS}
 
+singularity build \
+    ${DEST}/mriqc.simg \
+    docker://poldracklab/mriqc:${MRIQC_VERS}

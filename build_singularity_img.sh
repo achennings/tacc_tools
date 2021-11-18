e697d4e5a851ed02a805e63a87aaefd0ca790808
Augustin Hennings <ach3377@login1.frontera.tacc.utexas.edu>
2021-11-18 16:35:36 -0600

    latest fmriprep version

--- build_singularity_img.sh
+++ build_singularity_img.sh
@@ -4,21 +4,21 @@
 DEST=${SCRATCH}/bids-apps
 mkdir -p ${DEST}
 
-FMRIPREP_VERS=20.2.1
+FMRIPREP_VERS=20.2.6
 MRIQC_VERS=0.16.1
 #for all intents and purposes this should work but something is wrong with my python version and doesn't run for me, try it anyways...
 
 SINGULARITY_NOHTTPS=yes 
 
-echo DOING NOTHING, UNCOMMENT WHICH IMAGES TO BUILD IN THE FILE
-#singularity build \
-#    ${DEST}/fmriprep.simg \
-#    docker://nipreps/fmriprep:${FMRIPREP_VERS}
+#echo DOING NOTHING, UNCOMMENT WHICH IMAGES TO BUILD IN THE FILE
+singularity build \
+    ${DEST}/fmriprep.sif \
+    docker://nipreps/fmriprep:${FMRIPREP_VERS}
 
 #singularity build \
-#    ${DEST}/mriqc.simg \
+#    ${DEST}/mriqc.sif \
 #    docker://poldracklab/mriqc:${MRIQC_VERS}
 
 #singularity build \
-#    ${DEST}/neurodocker.simg \
+#    ${DEST}/neurodocker.sif \
 #    docker://achennings/neurodocker:latest
